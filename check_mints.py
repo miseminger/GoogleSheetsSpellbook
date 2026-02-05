dae5f683a61a092d5e1aec556197367e1f06987e
miseminger <madeline.iseminger@gmail.com>
2026-02-04 16:27:16 -0800

    Check mints sheet against GENEPIO-EDIT and identify matches with a new column

--- check_mints.py
+++ check_mints.py
@@ -18,18 +18,31 @@ from functions import *
 
 # The ID and range of the mints spreadsheet.
 MINTS_SPREADSHEET_ID = "1Ieo0jokfXBbWIQv32g5D5s7x8FIeh7f-gGX6qI6AhN0" #mints sheet
-SAMPLE_RANGE_NAME = "2025!A3:B" # fetch 'IRI' and 'Label' columns from the 2025 sheet
+SAMPLE_RANGE_NAME = "2025!A3:E" # fetch columns A-E from the 2025 sheet
+MINTS_COLUMNS = ['IRI', 'label', 'creator (GitHub username)', 'reservation date', 'subset'] # names of columns A-E in order
 
-# The absolute filepath of genepio-edit
-GENEPIO_EDIT = "/home/madeline/Desktop/git_temp/genepio/src/ontology/genepio-edit.owl"
 
 if __name__ == "__main__":
 
   # read 2025 mints sheet into a df
   mints_list = get_values(MINTS_SPREADSHEET_ID, SAMPLE_RANGE_NAME).get("values", [])
-  mints_df = pd.DataFrame(mints_list, columns=['IRI', 'Label']) # convert to a pandas df
-  mints_df = mints_df[mints_df['Label'].notna()] # remove rows with an IRI but no label
+  mints_df = pd.DataFrame(mints_list, columns=MINTS_COLUMNS) # convert to a pandas df
+  mints_df = mints_df[mints_df['label'].notna()] # remove rows with an IRI but no label
+  print("mints_df")
   print(mints_df)
 
   # read in TSV of terms that are already in GENEPIO
-  # this TSV must have columns titled ['IRI', 'Label']
+  # this TSV must have columns titled ['IRI', 'label']
+  genepio_edit_df = pd.read_csv("genepio_edit_terms.tsv", sep='\t', header=0)
+  genepio_edit_df['in_GENEPIO'] = 'YYYY' # add column to be used in merge
+  genepio_edit_df = genepio_edit_df.drop(columns=['type'])# drop 'type' column
+  print("genepio_edit_df")
+  print(genepio_edit_df)
+
+  # merge mints sheet with genepio-edit sheet
+  merged_df = pd.merge(mints_df, genepio_edit_df, on=['IRI', 'label'], how='left')
+  print("merged_df")
+  print(merged_df)
+  # final df should be the same size as mints_df!
+
+  print(merged_df[merged_df['in_GENEPIO'].notna()])
