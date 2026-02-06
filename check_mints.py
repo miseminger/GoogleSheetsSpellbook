937d4f497eef694547ad0da2609d17e475b583de
miseminger <madeline.iseminger@gmail.com>
2026-02-06 14:58:01 -0800

    add curation sheet tabs checks

--- check_mints.py
+++ check_mints.py
@@ -17,26 +17,34 @@ from googleapiclient.errors import HttpError
 from functions import *
 
 
-# The ID and range of the mints spreadsheet.
+# The ID and range of the Mints spreadsheet
+MINTS_SPREADSHEET_ID = "1Ieo0jokfXBbWIQv32g5D5s7x8FIeh7f-gGX6qI6AhN0" # mints sheet id
+MINTS_RANGE_NAMES = "2023!A2:E" #["2026", "2025"] #, "2024!A2:E", "2023!A2:E", "2022!A2:E"] # fetch columns A-E from the 2022-2026 sheets
+MINTS_COLUMN_NAMES = ['IRI', 'label', 'creator (GitHub username)', 'reservation date', 'subset'] # names of columns A-E in order
 
-MINTS_SPREADSHEET_ID = "1Ieo0jokfXBbWIQv32g5D5s7x8FIeh7f-gGX6qI6AhN0" # mints sheet
-MINTS_RANGE_NAME = "2025!A3:E" # fetch columns A-E from the 2025 sheet
-MINTS_COLUMNS = ['IRI', 'label', 'creator (GitHub username)', 'reservation date', 'subset'] # names of columns A-E in order
-
+# GENEPIO ROBOT Table
 GENEPIO_ROBOT_SPREADSHEET_ID = "1L1051tGcWerbCJkFPnBTe6gQ_9sYuthvmNPNf7Ljtq4" # ROBOT sheet
 GENEPIO_ROBOT_RANGE_NAME = "spec_field (new)!A4:B" # fetch first two columns
 GENEPIO_ROBOT_ARCHIVAL_RANGE_NAMES = ''
 
-CURATION_SHEET_2023 = "1s6FB9EyPWYBgssIU9a9AKATEfZnMdixwvrGhBXUHAVk" # GENEPIO 2023 Curation Tables
-CURATION_SHEET_2024 = "14r_qlNJUCGJ_59MA3MR0x4JObsYpQI_2AFJGMu_wyl4" # GENEPIO 2024 Curation Tables
+# GENEPIO 2023 Curation Tables
+CURATION_SHEET_2023 = "1s6FB9EyPWYBgssIU9a9AKATEfZnMdixwvrGhBXUHAVk" 
+CURATION_SHEET_2023_RANGE_NAMES = ["2023_mints_wastewater_terms (Charlie)", "2023_manual_curation (Charlie)"] # don't search "2023_mints_wastewater (Charlie)" until further notice
+CURATION_SHEET_2023_COLUMN_NAMES = ['Ontology ID','label', 'alternative label']
+
+# GENEPIO 2024 Curation Tables
+CURATION_SHEET_2024 = "14r_qlNJUCGJ_59MA3MR0x4JObsYpQI_2AFJGMu_wyl4" 
+CURATION_SHEET_2024_RANGE_NAMES = ["2024_hAMRonization", "2024_GRDI", "2024_HPAI", "2024_QC", "2024_CanCOGen", "2024_Pathoplexus", "2024_MPOX", "2024_wastewater"]
+CURATION_SHEET_2024_COLUMN_NAMES = ['Ontology ID','label', 'alternative label']
 
 
 if __name__ == "__main__":
 
-  # read 2025 mints sheet into a df
-  mints_list = get_values(MINTS_SPREADSHEET_ID, MINTS_RANGE_NAME).get("values", [])
-  mints_df = pd.DataFrame(mints_list, columns=MINTS_COLUMNS) # convert to a pandas df
+  # read 2023 mints sheet into a df
+  mints_list = get_values(MINTS_SPREADSHEET_ID, MINTS_RANGE_NAMES).get("values", [])
+  mints_df = pd.DataFrame(mints_list, columns=MINTS_COLUMN_NAMES) # convert to a pandas df
   mints_df = mints_df[mints_df['label'].notna()] # remove rows with an IRI but no label
+  #mints_df = get_multitab_df(MINTS_SPREADSHEET_ID, MINTS_RANGE_NAMES, MINTS_COLUMN_NAMES)
   print("mints_df")
   print(mints_df)
 
@@ -45,14 +53,14 @@ if __name__ == "__main__":
   genepio_edit_df = pd.read_csv("genepio_edit_terms.tsv", sep='\t', header=0)
   genepio_edit_df['In genepio-edit.owl?'] = 'YES' # add column to be used in merge
   genepio_edit_df = genepio_edit_df.drop(columns=['type'])# drop 'type' column
-  print("genepio_edit_df")
-  print(genepio_edit_df)
+  #print("genepio_edit_df")
+  #print(genepio_edit_df)
 
   # merge mints sheet with genepio-edit sheet
-  merged_df = pd.merge(mints_df, genepio_edit_df, on=['IRI', 'label'], how='left')
+  mints_review_df = pd.merge(mints_df, genepio_edit_df, on=['IRI', 'label'], how='left')
   # final df should be the same size as mints_df!
 
-  print(merged_df[merged_df['In genepio-edit.owl?'].notna()])
+  #print(merged_df[merged_df['In genepio-edit.owl?'].notna()])
 
   # read in genepio-ROBOT sheet: spec_field (new)
   robot_list = get_values(GENEPIO_ROBOT_SPREADSHEET_ID, GENEPIO_ROBOT_RANGE_NAME).get("values", [])
@@ -63,41 +71,50 @@ if __name__ == "__main__":
 
   # check if mints are in genepio-ROBOT sheet: spec_field (new)
   robot_df['In GENEPIO ROBOT?'] = 'YES' # add column to be used in merge
+  robot_df['Tab location in GENEPIO ROBOT'] = 'Coming soon!'
   # merge mints sheet with genepio-ROBOT sheet
-  merged_df_2 = pd.merge(merged_df, robot_df, on=['IRI', 'label'], how='left')
-  print("merged_df_2")
-  print(merged_df_2)
+  mints_review_df = pd.merge(mints_review_df, robot_df, on=['IRI', 'label'], how='left')
+  #print("mints_review_df")
+  #print(mints_review_df)
   # final df should be the same size as mints_df!
 
-  # print out any terms that are in the ROBOT file
-  print(merged_df_2[merged_df_2['In GENEPIO ROBOT?'].notna()])
+  ### check if mints are in genepio-ROBOT archival sheets
 
-  # replace all NaN values with empty strings
-  merged_df_2 = merged_df_2.fillna('')
+  # print out any terms that are in the ROBOT file
+  #print("terms in ROBOT spec field (new)")
+  #print(merged_df_2[merged_df_2['In GENEPIO ROBOT?'].notna()])
+
+  ## check if mints are in a curation sheet
+  curation_2023_df = get_multitab_df(CURATION_SHEET_2023, CURATION_SHEET_2023_RANGE_NAMES, CURATION_SHEET_2023_COLUMN_NAMES)
+  curation_2024_df = get_multitab_df(CURATION_SHEET_2024, CURATION_SHEET_2024_RANGE_NAMES, CURATION_SHEET_2024_COLUMN_NAMES)
+  # concatenate curation sheets into one long df
+  curation_df = pd.concat([curation_2023_df, curation_2024_df])
+  # add "In GENEPIO curation?" tab
+  curation_df["In GENEPIO curation?"] = 'YES'
+  # rename "Ontology ID" column to "IRI" to match mints sheet
+  curation_df = curation_df.rename(columns={"Ontology ID": "IRI", "tab": "Tab location in GENEPIO curation"})
+  # restrict columns to those that should be merged into mints_review
+  curation_df = curation_df[["IRI", "label", "In GENEPIO curation?", "Tab location in GENEPIO curation"]]
+  # combine duplicate rows and transform "Tab location in GENEPIO curation" into a comma-separated list
+  curation_df = curation_df.groupby(["IRI", "label", "In GENEPIO curation?"]).agg({"Tab location in GENEPIO curation": ', '.join}).reset_index()
+  #print(curation_df[curation_df["Tab location in GENEPIO curation"].str.contains(",")])
+
+  # take out problematic rows to look at separately
+  # take all rows with IRIs that don't contain "GENEPIO"
+  # take all rows without labels
+
+  # merge with mints_review
+  mints_review_df = pd.merge(mints_review_df, curation_df, on=['IRI', 'label'], how='left')
+
+  ## troubleshooting "2023_mints_wastewater (Charlie)"
+  #curation_2023_df = curation_2023_df.sort_values(by=["Ontology ID"])
+  #curation_2023_values = curation_2023_df.values.tolist()
+  #update_values(CURATION_SHEET_2023, "Madeline_error_review_2026", "RAW", curation_2023_values)
 
   # update the Mints_review spreadsheet in Google Sheets
-  values2 = merged_df_2.values.tolist() # convert df back to nested list
-  print(values2)
-  update_values(MINTS_SPREADSHEET_ID, "Mints review!A3:K", "RAW", values2)
-
-  # get multiple sheets within CURATION_SHEET_2023 into one large df
-  values = batch_get_values(CURATION_SHEET_2023, ["2023_to_request_ontologies (Charlie)", "2023_mints_wastewater (Charlie)"])
-  # initialize empty df
-  curation_2023_df = pd.DataFrame(columns=['Ontology ID','label', 'alternative label', 'tab', 'spreadsheet_id'])
-  for sheet in range(len(values['valueRanges'])):
-      tab = values['valueRanges'][sheet]['range'].split('!')[0] # save the name of the tab, eg. '2023_error_curation (Charlie)'
-      #print(tab)
-      # collect the data in that tab in a pandas df
-      sheet_data = values['valueRanges'][sheet]['values']
-      sheet_df = pd.DataFrame(sheet_data, columns=sheet_data[0])
-      sheet_df = sheet_df[2:] # leave off ROBOT instructions
-      #print(sheet_df.columns)
-      # only keep ['Ontology ID','label', 'alternative label'] columns
-      sheet_df = sheet_df[['Ontology ID','label', 'alternative label']]
-      # add a column showing the tab name
-      sheet_df["tab"] = tab
-      sheet_df["spreadsheet_id"] = CURATION_SHEET_2023
-      #print(sheet_df)
-      # append sheet_df to the spreadsheet df
-      curation_2023_df = pd.concat([curation_2023_df, sheet_df])
-  print(curation_2023_df)
+  # replace all NaN values with empty strings
+  mints_review_df = mints_review_df.fillna('')
+  print(mints_review_df)
+  print(mints_df)
+  mints_review_df_values = mints_review_df.values.tolist() # convert df back to nested list
+  update_values(MINTS_SPREADSHEET_ID, "Mints review!A3:K", "RAW", mints_review_df_values)
